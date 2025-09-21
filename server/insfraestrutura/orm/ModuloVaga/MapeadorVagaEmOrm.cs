4d700f77d59debd9444c288d62fd9d28087eaaaa
iagodizlla <iagoschlemper@gmail.com>
2025-09-21 19:15:34 -0300

    Implementado Vaga em ORM

---
+++ server/insfraestrutura/orm/ModuloVaga/MapeadorVagaEmOrm.cs
@@ -0,0 +1,20 @@
+﻿using GestaoDeEstacionamento.Core.Dominio.ModuloVaga;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GestaoDeEstacionamento.Infraestrutura.Orm.ModuloVaga;
+public class MapeadorVagaEmOrm : IEntityTypeConfiguration<Vaga>
+{
+    public void Configure(EntityTypeBuilder<Vaga> builder)
+    {
+        builder.Property(x => x.Id)
+            .ValueGeneratedNever()
+            .IsRequired();
+
+        builder.Property(x => x.Numero)
+            .IsRequired();
+
+        builder.Property(x => x.EstaOcupada)
+            .IsRequired();
+    }
+}
