93c1b6d2581b2a7044eda343f1e65f4ec6e66460
iagodizlla <iagoschlemper@gmail.com>
2025-09-21 19:18:09 -0300

    Implementado AutoMapper de Vaga em WebApi

---
+++ server/web-api/AutoMapper/VagaModelsMappingProfile.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using GestaoDeEstacionamento.Core.Aplicacao.ModuloVaga.Commands;
+using GestaoDeEstacionamento.WebApi.Models.ModuloVaga;
+using System.Collections.Immutable;
+
+namespace GestaoDeEstacionamento.WebApi.AutoMapper;
+
+public class VagaModelsMappingProfile : Profile
+{
+    public VagaModelsMappingProfile()
+    {
+        CreateMap<CadastrarVagaRequest, CadastrarVagaCommand>();
+        CreateMap<CadastrarVagaResult, CadastrarVagaResponse>();
+
+        CreateMap<(Guid, EditarVagaRequest), EditarVagaCommand>()
+            .ConvertUsing(src => new EditarVagaCommand(
+                src.Item1,
+                src.Item2.Numero,
+                src.Item2.EstaOcupada
+            ));
+
+        CreateMap<EditarVagaResult, EditarVagaResponse>();
+
+        CreateMap<Guid, ExcluirVagaCommand>()
+            .ConstructUsing(src => new ExcluirVagaCommand(src));
+
+        CreateMap<SelecionarVagaPorIdResult, SelecionarVagaPorIdResponse>()
+            .ConvertUsing(src => new SelecionarVagaPorIdResponse(
+                src.Id,
+                src.Numero,
+                src.EstaOcupada
+            ));
+
+        CreateMap<SelecionarVagasRequest, SelecionarVagasQuery>();
+
+        CreateMap<SelecionarVagasResult, SelecionarVagasResponse>()
+       .ConvertUsing((src, dest, ctx) => new SelecionarVagasResponse(
+           src.Vagas.Count,
+           src?.Vagas.Select(c => ctx.Mapper.Map<SelecionarVagasDto>(c)).ToImmutableList() ?? ImmutableList<SelecionarVagasDto>.Empty
+       ));
+    }
+}
